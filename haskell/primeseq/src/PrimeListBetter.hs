4dc0d916828452326bfd54052d30de391ebf3d81
Thiago Mata <thiago.henrique.mata@gmail.com>
2020-10-17 01:34:22 +1100

    adding new properties

--- haskell/primeseq/src/PrimeListBetter.hs
+++ haskell/primeseq/src/PrimeListBetter.hs
@@ -1,8 +1,15 @@
 module PrimeListBetter
-    ( infiniteFusePrimeListBetter ) where
+    ( 
+        infiniteFusePrimeListBetter,
+        infiniteSequenceLen,
+        infiniteSequenceSum,
+        infiniteSequenceProportion,
+        sequenceProportionBetter,
+    ) where
 
 import MultiplesBetter
 import Data.List
+import Data.Number.CReal 
 
 
 
@@ -20,3 +27,27 @@ initialPrimes = [2]
 infiniteFusePrimeListBetter =  initialPrimes ++ recursivePrimeList initialPrimes
 
 
+sequenceLen :: [Integer] -> Integer -> [Integer]
+sequenceLen (p:primes) previous = [currentValue] ++ (sequenceLen primes currentValue) where
+        currentValue = previous * ( p - 1 )
+
+
+infiniteSequenceLen = sequenceLen infiniteFusePrimeListBetter 1
+
+sequenceSum :: [Integer] -> Integer -> [Integer]
+sequenceSum (p:primes) previous = [currentValue] ++ (sequenceSum primes currentValue) where
+        currentValue = previous * ( p )
+
+infiniteSequenceSum = sequenceSum infiniteFusePrimeListBetter 1
+
+
+sequenceProportion :: [Integer] -> [Integer] -> [CReal]
+sequenceProportion (s:seqSum) (l:seqL) = [ fromInteger(l) / fromInteger(s) ] ++ (sequenceProportion seqSum seqL) 
+
+infiniteSequenceProportion = sequenceProportion infiniteSequenceSum infiniteSequenceLen
+
+sequenceProportionBetter :: [Integer] -> CReal -> [CReal]
+sequenceProportionBetter (p:primes) previous = [currentValue] ++ (sequenceProportionBetter primes currentValue) where
+        currentValue =  previous * fromInteger(p-1) /fromInteger(p)
+
+-- infiniteSequenceProportionBetter = sequenceProportionBetter infiniteFusePrimeListBetter 1
