90184f5b237955f944ebd4d395256d04827cf327
Thiago Mata <thiago.mata@rokt.com>
2024-11-05 08:54:29 +1100

    improve summary

--- src/main/scala/v1/div/properties/Summary.scala
+++ src/main/scala/v1/div/properties/Summary.scala
@@ -2,6 +2,7 @@ package v1.properties
 
 import stainless.lang.*
 import stainless.proof.check
+import v1.Calc.{div, mod}
 import v1.div.properties.ModOperations
 import v1.{Calc, DivMod}
 
@@ -16,13 +17,13 @@ object Summary {
       check(a >= 0)
       check(ModSmallDividend.modSmallDividend(a, b))
       check(ModIdempotence.modIdempotence(a, b))
-      check(Calc.mod(a, b) == a)
-      check(Calc.div(a, b) == 0)
+      check(mod(a, b) == a)
+      check(div(a, b) == 0)
     }
 
     check(ModIdentity.modIdentity(b))
-    check(Calc.mod(b, b) == 0)
-    check(Calc.div(b, b) == 1)
+    check(mod(b, b) == 0)
+    check(div(b, b) == 1)
 
     check(AdditionAndMultiplication.APlusBSameModPlusDiv(a, b))
     check(AdditionAndMultiplication.ALessBSameModDecreaseDiv(a, b))
@@ -32,32 +33,32 @@ object Summary {
     check(m >= 0)
     check(AdditionAndMultiplication.ALessMultipleTimesBSameMod(a, b, m))
     check(AdditionAndMultiplication.APlusMultipleTimesBSameMod(a, b, m))
-    check(Calc.mod( a + b * m, b ) == Calc.mod( a, b ))
+    check(mod( a + b * m, b ) == mod( a, b ))
 
     check(b != 0)
     check(ModOperations.modAdd(a, b, c))
-    check(Calc.div(a + c, b) == Calc.div(a, b) + Calc.div(c, b) + Calc.div(Calc.mod(a, b) + Calc.mod(c, b), b))
-    check(Calc.mod(a + c, b) == Calc.mod(Calc.mod(a, b) + Calc.mod(c, b), b))
+    check(div(a + c, b) == div(a, b) + div(c, b) + div(mod(a, b) + mod(c, b), b))
+    check(mod(a + c, b) == mod(mod(a, b) + mod(c, b), b))
 
     check(ModOperations.modLess(a, b, c))
-    check(Calc.mod(a - c, b) == Calc.mod(Calc.mod(a, b) - Calc.mod(c, b), b))
-    check(Calc.div(a - c, b) == Calc.div(a, b) - Calc.div(c, b) + Calc.div(Calc.mod(a, b) - Calc.mod(c, b), b))
+    check(mod(a - c, b) == mod(mod(a, b) - mod(c, b), b))
+    check(div(a - c, b) == div(a, b) - div(c, b) + div(mod(a, b) - mod(c, b), b))
 
 
-    ( if a >= 0 && b > a then Calc.div(a,b) == 0 else true )           &&
-    ( if a >= 0 && b > a then Calc.mod(a,b) == a else true )           &&
-    Calc.mod( b, b )     == 0                                          &&
-    Calc.div( b, b )     == 1                                          &&
-    Calc.mod( a + b * m, b ) == Calc.mod( a, b )                       &&
-    Calc.mod( a - b * m, b ) == Calc.mod( a, b )                       &&
-    Calc.mod(Calc.mod(a, b), b) == Calc.mod( a, b )                    &&
-    Calc.div( a + b, b ) == Calc.div( a, b ) + 1                       &&
-    Calc.div( a - b, b ) == Calc.div( a, b ) - 1                       &&
-    Calc.div( a + b * m, b ) == Calc.div( a, b ) + m                   &&
-    Calc.div( a - b * m, b ) == Calc.div( a, b ) - m                   &&
-    Calc.mod(a + c, b) == Calc.mod(Calc.mod(a, b) + Calc.mod(c, b), b) &&
-    Calc.mod(a - c, b) == Calc.mod(Calc.mod(a, b) - Calc.mod(c, b), b) &&
-    Calc.div(a + c, b) == Calc.div(a, b) + Calc.div(c, b) + Calc.div(Calc.mod(a, b) + Calc.mod(c, b), b) &&
-    Calc.div(a - c, b) == Calc.div(a, b) - Calc.div(c, b) + Calc.div(Calc.mod(a, b) - Calc.mod(c, b), b)
+    ( if a >= 0 && b > a then div(a,b) == 0 else true )           &&
+    ( if a >= 0 && b > a then mod(a,b) == a else true )           &&
+    mod( b, b )     == 0                                          &&
+    div( b, b )     == 1                                          &&
+    mod( a + b * m, b ) == mod( a, b )                       &&
+    mod( a - b * m, b ) == mod( a, b )                       &&
+    mod(mod(a, b), b) == mod( a, b )                    &&
+    div( a + b, b ) == div( a, b ) + 1                       &&
+    div( a - b, b ) == div( a, b ) - 1                       &&
+    div( a + b * m, b ) == div( a, b ) + m                   &&
+    div( a - b * m, b ) == div( a, b ) - m                   &&
+    mod(a + c, b) == mod(mod(a, b) + mod(c, b), b) &&
+    mod(a - c, b) == mod(mod(a, b) - mod(c, b), b) &&
+    div(a + c, b) == div(a, b) + div(c, b) + div(mod(a, b) + mod(c, b), b) &&
+    div(a - c, b) == div(a, b) - div(c, b) + div(mod(a, b) - mod(c, b), b)
   }.holds
 }
