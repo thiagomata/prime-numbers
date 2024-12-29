3324aea8882c868796a943f3ac8269074e0a4f1d
Thiago Mata <thiago.henrique.mata@gmail.com>
2024-12-29 12:36:35 -0300

    create installer

---
+++ install.sh
@@ -0,0 +1,6 @@
+rm -f sbt-stainless.zip
+wget https://github.com/epfl-lara/stainless/releases/download/v0.9.8.9/sbt-stainless.zip
+rm -rf project
+rm -rf stainless
+unzip sbt-stainless.zip
+rm -f sbt-stainless.zip
