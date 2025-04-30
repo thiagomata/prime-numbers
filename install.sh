3aa871c25a993a7f04c3dacf011d73397f324609
Thiago Mata <thiago.henrique.mata@gmail.com>
2025-04-30 18:52:43 +1000

    donwload the correct file based on the OS

--- install.sh
+++ install.sh
@@ -5,11 +5,46 @@ rm -rf project
 rm -rf stainless
 unzip sbt-stainless.zip
 
-if [ ! -f "stainless-dotty-standalone-0.9.8.8.zip" ]; then
-  wget https://github.com/epfl-lara/stainless/releases/download/v0.9.8.8/stainless-dotty-standalone-0.9.8.8-linux.zip
-  mv stainless-dotty-standalone-0.9.8.8-linux.zip stainless-dotty-standalone-0.9.8.8.zip
+VERSION="0.9.8.8"
+BASE_URL="https://github.com/epfl-lara/stainless/releases/download/v$VERSION"
+TARGET="stainless-dotty-standalone-$VERSION.zip"
+
+# Detect platform
+OS="$(uname -s)"
+ARCH="$(uname -m)"
+
+# Check if the file already exists
+if [ -f "$TARGET" ]; then
+   echo "$FILE already exists. Skipping download."
+else
+   # Determine the correct suffix
+  case "$OS" in
+      Linux)
+          FILE="stainless-dotty-standalone-$VERSION-linux.zip"
+          ;;
+      Darwin)
+          if [[ "$ARCH" == "arm64" ]]; then
+              FILE="stainless-dotty-standalone-$VERSION-mac-arm64.zip"
+          else
+              echo "Unsupported Mac architecture: $ARCH"
+              exit 1
+          fi
+          ;;
+      MINGW*|MSYS*|CYGWIN*|Windows_NT)
+          FILE="stainless-dotty-standalone-$VERSION-win.zip"
+          ;;
+      *)
+          echo "Unsupported OS: $OS"
+          exit 1
+          ;;
+  esac
+
+  # Download the file
+  echo "Downloading $FILE..."
+  wget "$BASE_URL/$FILE" -O "$TARGET"
 fi
+
 rm -rf stainless-dotty-standalone-0.9.8.8
-unzip stainless-dotty-standalone-0.9.8.8.zip -d stainless-dotty-standalone-0.9.8.8
+unzip "${TARGET}" -d stainless-dotty-standalone-0.9.8.8
 rm -rf project/lib/stainess-library.jar
 cp ./stainless-dotty-standalone-0.9.8.8/lib/stainless-library.jar project/lib/
