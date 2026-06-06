a2daae26d4326e43c220567e2fa8ac6366838c2f
Thiago Mata <thiago.henrique.mata@gmail.com>
2026-06-07 00:59:20 +1000

    seq v5 - cycle rotateAt(k).apply(i) == apply(k + i).

--- src/main/scala/v1/cycle/recursive/RecursiveCycle.scala
+++ src/main/scala/v1/cycle/recursive/RecursiveCycle.scala
@@ -4,6 +4,9 @@ import stainless.collection.List
 import stainless.lang.BooleanDecorations
 import stainless.lang.decreases
 import v1.cycle.CycleUtils
+import v1.cycle.mod.ModCycle
+import v1.cycle.properties.CycleProperties
+import v1.cycle.recursive.properties.RecursiveCycleMatchesModCycle
 import v1.list.ListUtils
 import v1.list.properties.ListUtilsProperties
 
@@ -96,5 +99,25 @@ case class RecursiveCycle(values: List[BigInt]) {
       assert(cycleValueBiggerThan(pos - size, x))
       apply(pos) > x
     }
+  }.holds
+
+  def rotateAtValue(k: BigInt, i: BigInt): Boolean = {
+    require(k >= 0)
+    require(i >= 0)
+
+    val modCycle = ModCycle(values)
+    val rotated = CycleUtils.collectRotated(values, k, size)
+    val rotatedModCycle = ModCycle(rotated)
+
+    CycleProperties.rotateAtValue(modCycle, k, i)
+    assert(rotatedModCycle(i) == modCycle(k + i))
+
+    RecursiveCycleMatchesModCycle.assertCycleAndRecursiveCycleMathForAnyValues(modCycle, k + i)
+    assert(modCycle(k + i) == apply(k + i))
+
+    RecursiveCycleMatchesModCycle.assertCycleAndRecursiveCycleMathForAnyValues(rotatedModCycle, i)
+    assert(rotatedModCycle(i) == rotateAt(k).apply(i))
+
+    rotateAt(k).apply(i) == apply(k + i)
   }.holds
 }
