5c281f335113ef3767436f406a796b85b15a34f0
Thiago Mata <thiago.henrique.mata@gmail.com>
2026-06-26 15:47:23 +1000

    repeated list properties

--- src/main/scala/v1/chapter3/list/RepeatedList.scala
+++ src/main/scala/v1/chapter3/list/RepeatedList.scala
@@ -3,6 +3,7 @@ package v1.chapter3.list
 import stainless.collection.List
 import stainless.lang.*
 import v1.chapter2.div.Calc
+import v1.chapter3.list.ListUtils
 
 case class RepeatedList(original: List[BigInt], nTimes: BigInt) {
   require(original.nonEmpty)
@@ -10,15 +11,22 @@ case class RepeatedList(original: List[BigInt], nTimes: BigInt) {
 
   def size: BigInt = original.size * nTimes
 
-  def toValues: List[BigInt] = {
-    decreases(nTimes)
-    if (nTimes == 1) original
-    else original ++ RepeatedList(original, nTimes - 1).toValues
-  }
-
   def apply(index: BigInt): BigInt = {
     require(index >= 0)
     require(index < size)
     original(Calc.mod(index, original.size))
+  }.ensuring(result =>
+    result == original(Calc.mod(index, original.size)))
+
+  def toValues: List[BigInt] = {
+    decreases(nTimes)
+    if (nTimes == 1) original
+    else original ++ RepeatedList(original, nTimes - 1).toValues
+  }.ensuring(result =>
+    result.size == original.size * nTimes &&
+    result.nonEmpty)
+
+  def sum: BigInt = {
+    ListUtils.sum(toValues)
   }
 }
--- src/main/scala/v1/chapter3/list/properties/RepeatedListProperties.scala
+++ src/main/scala/v1/chapter3/list/properties/RepeatedListProperties.scala
@@ -5,7 +5,60 @@ import stainless.lang.*
 import v1.chapter1.verification.Helper.assert
 import v1.chapter2.div.Calc
 import v1.chapter3.list.RepeatedList
+import v1.chapter3.list.ListUtils
+import v1.chapter3.list.properties.ListUtilsProperties
 
 object RepeatedListProperties {
+
+  def assertSumBase(
+    list: List[BigInt]
+  ): Boolean = {
+    require(list.nonEmpty)
+    RepeatedList(list, 1).sum == ListUtils.sum(list)
+  }.holds
+
+  def assertSumStep(
+    list: List[BigInt],
+    times: BigInt
+  ): Boolean = {
+    require(list.nonEmpty)
+    require(times > 1)
+    val current = RepeatedList(list, times)
+    val prev = RepeatedList(list, times - 1)
+    ListUtilsProperties.listCombine(list, prev.toValues)
+    current.sum == ListUtils.sum(list) + prev.sum
+  }.holds
+
+  def assertSumMultiplier(
+    list: List[BigInt],
+    times: BigInt
+  ): Boolean = {
+    require(list.nonEmpty)
+    require(times > 0)
+    decreases(times)
+    val totalSum = ListUtils.sum(list)
+    if (times == 1) {
+      assertSumBase(list)
+    } else {
+      assertSumMultiplier(list, times - 1)
+      assertSumStep(list, times)
+    }
+    RepeatedList(list, times).sum == times * totalSum
+  }.holds
+
+  def assertElementNotMultiple(
+    list: List[BigInt],
+    times: BigInt,
+    filterValue: BigInt,
+    index: BigInt
+  ): Boolean = {
+    require(list.nonEmpty)
+    require(times > 0)
+    require(filterValue > 0)
+    require(index >= 0)
+    require(index < list.size * times)
+    require(Calc.mod(list(Calc.mod(index, list.size)), filterValue) != BigInt(0))
+    Calc.mod(RepeatedList(list, times).apply(index), filterValue) != BigInt(0)
+  }.holds
 
 }
